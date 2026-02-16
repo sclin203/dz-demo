60f9e3467956d5d88e649fe75fce648c924c8814
Katya <dancevae0@gmail.com>
2026-02-16 14:20:57 +0300

    final

---
+++ script6.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Привет"
+echo "Моё имя $USER"
+echo "Сейчас я нахожусь в $(pwd)"
+echo "Мой домашний каталог $HOME"
+echo "Моя ОС"
+cat /etc/os-release
+echo "Мое время работы"
+uptime
